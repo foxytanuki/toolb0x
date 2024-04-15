985ace21eab5ebf9b20cb42a6bfa7228aa416c6d
foxytanuki <biz@foxytanuki.dev>
2024-04-16 00:20:15 +0900

    Update ChainContext to use localStorage

--- src/contexts/ChainContext.tsx
+++ src/contexts/ChainContext.tsx
@@ -1,4 +1,10 @@
-import { createContext, useContext, useState, ReactNode } from "react";
+import {
+  createContext,
+  useContext,
+  useState,
+  ReactNode,
+  useEffect,
+} from "react";
 import { Chain, mainnet } from "viem/chains";
 
 interface ChainContextType {
@@ -9,7 +15,17 @@ interface ChainContextType {
 const ChainContext = createContext<ChainContextType | undefined>(undefined);
 
 export const ChainProvider = ({ children }: { children: ReactNode }) => {
-  const [chain, setChain] = useState<Chain>(mainnet);
+  const [chain, setChain] = useState<Chain>(() => {
+    const chain = localStorage.getItem("chain");
+    if (chain) {
+      return JSON.parse(chain);
+    }
+    return mainnet;
+  });
+
+  useEffect(() => {
+    localStorage.setItem("chain", JSON.stringify(chain));
+  }, [chain]);
 
   return (
     <ChainContext.Provider value={{ chain, setChain }}>
