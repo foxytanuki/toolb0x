263060be3116a523e71c4375d477dd89439f0408
foxytanuki <biz@foxytanuki.dev>
2024-04-17 13:06:53 +0900

    Add Transaction Fee Calculator

--- src/components/FeeHistorySearch.tsx
+++ src/components/FeeHistorySearch.tsx
@@ -1,4 +1,13 @@
-import { Box, Text, VStack, useColorMode } from "@chakra-ui/react";
+import { useEffect, useState } from "react";
+import {
+  Box,
+  Text,
+  VStack,
+  useColorMode,
+  FormControl,
+  FormLabel,
+  Input,
+} from "@chakra-ui/react";
 import { useFeeHistory } from "../hooks/useFeeHistory";
 import { usePolygonGasStation } from "../hooks/usePolygonGasStation";
 import { formatGwei } from "viem";
@@ -7,6 +16,8 @@ import { useChain } from "../hooks/useChain";
 import { useGasPrice } from "../hooks/useGasPrice";
 
 const FeeHistorySearch = () => {
+  const [gasUsage, setGasUsage] = useState<bigint>(5760000n);
+  const [fee, setFee] = useState<bigint>(0n);
   const { colorMode } = useColorMode();
   const { chain } = useChain();
   const {
@@ -27,77 +38,108 @@ const FeeHistorySearch = () => {
   const priorityFeePerGas = feeHistory?.rewards[0] ?? 0n;
   const totalFeePerGas = baseFeePerGas + priorityFeePerGas;
 
+  useEffect(() => {
+    if (gasPrice) {
+      const fee = (gasUsage * gasPrice) / 1000000000n;
+      setFee(fee);
+    }
+  }, [gasPrice, gasUsage]);
+
   return (
-    <Box
-      as="main"
-      w="98%"
-      maxW={["container.sm", "container.md", "container.lg"]}
-      mx="auto"
-      p={[4, 6, 8]}
-      bg={colorMode === "light" ? "white" : "gray.800"}
-      borderRadius="md"
-      boxShadow="base"
-    >
-      <VStack spacing={4} align="stretch">
-        <Box fontSize="xx-large" fontWeight="bold">
-          Gas Fee Dashboard
-        </Box>
-        {isLoading && <LoadingSpinner />}
-        {error && <Text color="red.500">Error: {error.message}</Text>}
-        {feeHistory && (
-          <VStack spacing={3} align="stretch">
-            <Box>
-              <Box as="h3" fontSize="xl" fontWeight="extrabold">
-                eth_feeHistory
-              </Box>
-              <Text>
-                <strong>BaseFeePerGas:</strong> {formatGwei(baseFeePerGas)}Gwei
-              </Text>
-              <Text>
-                <strong>Rewards:</strong> {formatGwei(priorityFeePerGas)}Gwei
-              </Text>
-              <Text>
-                <strong>Total:</strong> {formatGwei(totalFeePerGas)}Gwei
-              </Text>
-            </Box>
-            {gasPriceLoading && <LoadingSpinner />}
-            {gasPrice?.toString && (
+    <>
+      <Box
+        as="main"
+        w="98%"
+        maxW={["container.sm", "container.md", "container.lg"]}
+        mx="auto"
+        p={[4, 6, 8]}
+        bg={colorMode === "light" ? "white" : "gray.800"}
+        borderRadius="md"
+        boxShadow="base"
+      >
+        <VStack spacing={4} align="stretch">
+          <Box fontSize="xx-large">Gas Fee Dashboard</Box>
+          {isLoading && <LoadingSpinner />}
+          {error && <Text color="red.500">Error: {error.message}</Text>}
+          {feeHistory && (
+            <VStack spacing={3} align="stretch">
               <Box>
-                <Box as="h3" fontSize="xl" fontWeight="extrabold">
-                  eth_gasPrice
+                <Box as="h3" fontSize="xl">
+                  eth_feeHistory
                 </Box>
-                <Text>
-                  <strong>GasPrice:</strong> {formatGwei(gasPrice)}Gwei
-                </Text>
+                <Text>BaseFeePerGas: {formatGwei(baseFeePerGas)} Gwei</Text>
+                <Text>Rewards: {formatGwei(priorityFeePerGas)} Gwei</Text>
+                <Text>Total: {formatGwei(totalFeePerGas)} Gwei</Text>
               </Box>
-            )}
-            {polygonGasStationLoading && <LoadingSpinner />}
-            {polygonGasStation && (
-              <Box>
-                <Box as="h3" fontSize="xl" fontWeight="extrabold">
-                  Polygon Gas Station (Fast)
+              {gasPriceLoading && <LoadingSpinner />}
+              {gasPrice?.toString && (
+                <Box>
+                  <Box as="h3" fontSize="xl">
+                    eth_gasPrice
+                  </Box>
+                  <Text>GasPrice: {formatGwei(gasPrice)}Gwei</Text>
                 </Box>
-                <Text>
-                  <strong>BaseFee:</strong>{" "}
-                  {(
-                    polygonGasStation.maxFee - polygonGasStation.maxPriorityFee
-                  ).toFixed(9)}
-                  Gwei
-                </Text>
-                <Text>
-                  <strong>MaxPriorityFee:</strong>{" "}
-                  {polygonGasStation.maxPriorityFee.toString()}Gwei
-                </Text>
-                <Text>
-                  <strong>MaxFee:</strong> {polygonGasStation.maxFee.toString()}
-                  Gwei
-                </Text>
-              </Box>
+              )}
+              {polygonGasStationLoading && <LoadingSpinner />}
+              {polygonGasStation && (
+                <Box>
+                  <Box as="h3" fontSize="xl">
+                    Polygon Gas Station (Fast)
+                  </Box>
+                  <Text>
+                    BaseFee:{" "}
+                    {(
+                      polygonGasStation.maxFee -
+                      polygonGasStation.maxPriorityFee
+                    ).toFixed(9)}
+                    Gwei
+                  </Text>
+                  <Text>
+                    MaxPriorityFee:{" "}
+                    {polygonGasStation.maxPriorityFee.toString()}Gwei
+                  </Text>
+                  <Text>
+                    MaxFee: {polygonGasStation.maxFee.toString()}
+                    Gwei
+                  </Text>
+                </Box>
+              )}
+            </VStack>
+          )}
+        </VStack>
+      </Box>
+      <Box
+        as="main"
+        w="100%"
+        maxW={["container.sm", "container.md", "container.lg"]}
+        mx="auto"
+        mt={4}
+        p={[4, 6, 8]}
+        bg={colorMode === "light" ? "white" : "gray.800"}
+        borderRadius="md"
+        boxShadow="base"
+      >
+        <VStack spacing={4} align="stretch">
+          <Box>
+            <Text fontSize="xx-large">Transaction Fee Calculator</Text>
+          </Box>
+          <Box>
+            <FormControl>
+              <FormLabel htmlFor="gasUsage">Gas Usage</FormLabel>
+              <Input
+                id="gasUsage"
+                type="number"
+                value={gasUsage.toString()}
+                onChange={(e) => setGasUsage(BigInt(e.target.value))}
+              />
+            </FormControl>
+            {fee !== null && (
+              <Text mt={4}>Transaction Fee: {formatGwei(fee)} MATIC</Text>
             )}
-          </VStack>
-        )}
-      </VStack>
-    </Box>
+          </Box>
+        </VStack>
+      </Box>
+    </>
   );
 };
 
